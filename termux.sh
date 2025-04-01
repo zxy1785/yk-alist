912c2f15c277a4745fbb661d1f3e058df189fe4f
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2025-04-01 16:15:20 +0800

    Create termux.sh

---
+++ termux.sh
@@ -0,0 +1,19 @@
+#!/data/data/com.termux/files/usr/bin/sh
+termux-change-repo
+pkg update
+echo '安装依赖'
+pkg instal termux-services tsu -y
+apt install -y wget dpkg
+echo '安装Alist'
+wget https://ghfast.top/https://github.com/ykxVK8yL5L/termux-packages/releases/latest/download/alist_1_aarch64.deb -O alist.deb
+dpkg -i alist.deb
+alist admin set admin
+echo '创建开机启动服务'
+mkdir -p $PREFIX/var/service/alist/log 
+ln -sf $PREFIX/share/termux-services/svlogger $PREFIX/var/service/alist/log/run
+echo '#!/data/data/com.termux/files/usr/bin/sh' > $PREFIX/var/service/alist/run
+echo 'exec 2>&1' >> $PREFIX/var/service/alist/run
+echo 'cd ~ && alist server' >> $PREFIX/var/service/alist/run
+chmod a+x $PREFIX/var/service/alist/run
+sv-enable alist 
+sv up alist 
