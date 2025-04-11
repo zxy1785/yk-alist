520cbcde096d927d8ba25beb5e3276b127ff264d
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2025-04-11 16:15:20 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,14 @@ umask ${UMASK}
 if [ "$1" = "version" ]; then
   ./alist version
 else
+
+ if [ "$RUN_ARIA2" = "true" ]; then
+    chown -R ${PUID}:${PGID} /opt/aria2/
+    exec su-exec ${PUID}:${PGID} nohup aria2c \
+      --enable-rpc \
+      --rpc-allow-origin-all \
+      --conf-path=/opt/aria2/.aria2/aria2.conf \
+      >/dev/null 2>&1 &
+  fi
   exec su-exec ${PUID}:${PGID} ./alist server --no-prefix
 fi
